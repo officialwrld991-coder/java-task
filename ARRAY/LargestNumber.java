a867940235e1d4bdc8a67c514a7e4e1a936a911b
officalwrld991-coder <officialwrld991@gmail.com>
2026-04-29 07:31:45 +0100

    array

---
+++ ARRAY/LargestNumber.java
@@ -0,0 +1,19 @@
+public class LargestNumber{
+
+    public class FuncArray(){
+        
+    }
+    
+    
+    public static void main(String[] args){
+       int []numbers = {98,100,83,50,93};
+
+        int largestNumber = numbers[0];
+        for(int index = 0; index < numbers.length; index++){
+            if(numbers[index] > largestNumber){
+                largestNumber = numbers[index];
+                System.out.println(largestNumber + " is the largest number");
+            }
+        } 
+    }
+}
